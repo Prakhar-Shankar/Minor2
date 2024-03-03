e88eebeca5f6ad74b1e58eda9d6ed0463797dacf
Prakhar-Shankar <prakharshankar247@gmail.com>
2024-03-04 00:37:48 +0530

    User Registration done

--- components/Registerpage.js
+++ components/Registerpage.js
@@ -1,116 +1,152 @@
-import React from 'react';
-import {
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Text,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, View, Text, Image, TouchableOpacity, TextInput, Alert } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../utils/firebaseConfig';
 
-import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import profile from "../images/profile.png"
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-
+import { Entypo, MaterialIcons, Ionicons } from '@expo/vector-icons';
+import profile from "../images/profile.png";
 import CustomButton from '../components/CustomButton';
-import InputField from '../components/InputField';
 
-const Registerpage = ({navigation}) => {
+const Registerpage = ({ navigation }) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+
+  const registerUser = async () => {
+    try {
+      // Check if passwords match
+      if (password !== confirmPassword) {
+        Alert.alert('Error', 'Passwords do not match.');
+        return;
+      }
+
+      // You can customize this to your needs
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log('User account created & signed in:', user);
+      // You can navigate to another screen here if needed
+    } catch (error) {
+      console.error('Error creating user:', error);
+      Alert.alert('Error', 'Failed to create user. Please try again.');
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{paddingHorizontal: 25}}>
-        <View style={{alignItems: 'center'}}>
-        <Image source={profile}
-            height={300}
-            width={300}
-            style={{/*transform: [{rotate: '-5deg'}],*/marginTop:'8'}}
+      <View style={styles.contentContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={profile} height={300} width={300} />
+        </View>
+        <Text style={styles.login}>Register</Text>
+        <View style={styles.inputContainer}>
+          <Ionicons name="person" size={20} color="#666" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            value={fullName}
+            onChangeText={setFullName}
+            placeholder="Full Name"
           />
         </View>
-
-        <Text
-          style={styles.login}>
-          Register
-        </Text>
-
-        <InputField
-          label={'Full Name'}
-          icon={
-            <Ionicons name="person" size={20} color="#464853" style={{marginRight: 8}}/>
-          }
-          inputType="text" 
-        />
-
-        <InputField
-          label={'Email ID'}
-          icon={
-            <MaterialIcons name="email" size={20} color="#464853" style={{marginRight: 8}}/>
-          }
-          keyboardType="email-address"
-        />
-
-        <InputField 
-          label={'Password'}
-          icon={
-            <Entypo name="lock" size={20} color="#464853" style={{marginRight: 8}}/> 
-          }
-          inputType="password" 
-        />
-        <InputField 
-          label={'Confirm Password'}
-          icon={
-            <Entypo name="lock" size={20} color="#464853" style={{marginRight: 8}}/> 
-          }
-          inputType="password" 
-        />
-        <InputField 
-          label={'Mobile Number'}
-          icon={
-            <FontAwesome name="phone" size={20} color="#464853" style={{marginRight: 8}}/>
-          }
-          inputType="number" 
-        />
-        
-        <CustomButton label={"Register"} onPress={() => {}} />
-
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginBottom: 30,
-          }}>
-          <Text style={{marginBottom:12}}>Already have an account ?</Text>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="email" size={20} color="#666" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Entypo name="lock" size={20} color="#666" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password"
+            secureTextEntry
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Entypo name="lock" size={20} color="#666" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            placeholder="Confirm Password"
+            secureTextEntry
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="phone" size={20} color="#666" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            value={mobileNumber}
+            onChangeText={setMobileNumber}
+            placeholder="Mobile Number"
+            keyboardType="phone-pad"
+          />
+        </View>
+        <CustomButton label={"Register"} onPress={registerUser} />
+        <View style={styles.registerContainer}>
+          <Text>Already have an account?</Text>
           <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={{color: '#AD40AF', fontWeight: '700',marginBottom:12}}> Login</Text>
+            <Text style={styles.registerText}> Login</Text>
           </TouchableOpacity>
-        </View> 
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
-export default Registerpage;
-
 const styles = StyleSheet.create({
-      container: {
-        flex: 1, justifyContent: 'center',backgroundColor:'white'
-        
-      },
-      login: {
-        fontFamily: 'arial',
-        fontSize: 30,
-        fontWeight: '800',
-        color: '#181816',
-        marginBottom: 30,
-      },
-      icon:{
-        borderColor: '#0066ff',
-        borderWidth: 2,
-        borderRadius: 10,
-        paddingHorizontal: 30,
-        paddingVertical: 10,
-      },
-    });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  contentContainer: {
+    paddingHorizontal: 25,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  login: {
+    fontFamily: 'arial',
+    fontSize: 30,
+    fontWeight: '800',
+    color: '#181816',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    paddingBottom: 8,
+    marginBottom: 25,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+  },
+  registerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  registerText: {
+    color: '#AD40AF',
+    fontWeight: '700',
+    marginLeft: 5,
+  },
+});
+
+export default Registerpage;
